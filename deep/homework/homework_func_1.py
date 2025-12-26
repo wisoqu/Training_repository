71033687a56d6c2d0af8d7d4a7ef80a0398df3b7
Mr. Optimist <georgiichurakov@outlook.com>
2025-12-26 12:05:03 +0700

    New homework is now availible!

---
+++ deep/homework/homework_func_1.py
@@ -0,0 +1,16 @@
+def fav_prcessor():
+    brand()
+    seria()
+    year()
+    performance()
+    cores()
+    isActual()
+
+def brand(): print('Intel')
+def seria(): print('I5-12450H')
+def year(): print(2022)
+def performance(): print('2 Ghz base can be upped to 4.4 Ghz')
+def cores(): print(8)
+def isActual(): print(True)
+
+fav_prcessor()
