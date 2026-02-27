0964791ede2d9a9ff73cb8f681b671342b4b40b2
Mr. Optimist <georgiichurakov@outlook.com>
2026-02-27 21:11:07 +0700

    PROPERTY COMMMIT

--- deep/classwork/polimorf.py
+++ deep/classwork/polimorf.py
@@ -1,5 +1,6 @@
 # # Полиморфизм
 #
+# Классический полиморфизм
 # class Animal:
 #     def create_sound(self):
 #         pass
@@ -22,30 +23,79 @@
 # animal = [Dog(), Cat(), Parrot()]
 # for x in animal:
 #     make_animal_sound(x)
+#
+# class Figure:
+#     def create_v(self):
+#         pass
+#
+# class Sphere(Figure):
+#     def create_v(self):
+#         return '(4/3) * 3.14159 * radius**3'
+#
+# class Cube(Figure):
+#     def create_v(self):
+#         return 'side**3'
+#
+# class Piramid(Figure):
+#     def create_v(self):
+#         return '(1/3) * base_area * height'
+#
+#
+# def show_info_v(figure: Figure):
+#     print(figure.create_v())
+#
+#
+#
+# figures = [Sphere(), Cube(), Piramid()]
+#
+# for _ in figures:
+#     show_info_v(_)
 
-class Figure:
-    def create_v(self):
-        pass
+# Параметрический полиморфизм
+#
+# from typing import TypeVar, List
+#
+# T = TypeVar("T")
+#
+#
+# def reverse_list(items: List[T]) -> List[T]:
+#     """Fucntion is returning the list with same data-type it got"""
+#     return items[::-1]
+#
+# nums = reverse_list([1, 2, 3])
+# lst_str = reverse_list(['a', 'b', 'c'])
+#
+# print(nums)
+# print(lst_str)
 
-class Sphere(Figure):
-    def create_v(self):
-        return '(4/3) * 3.14159 * radius**3'
+from typing import Generic, TypeVar, List
 
-class Cube(Figure):
-    def create_v(self):
-        return 'side**3'
+T = TypeVar("T")
 
-class Piramid(Figure):
-    def create_v(self):
-        return '(1/3) * base_area * height'
+class Storage(Generic[T]):
+    def __init__(self):
+        self.__items: List[T] = []
+
+    def put(self, item: T):
+        self.__items.append(item)
+        print(f"Добавлено: {item}")
+
+    @property
+    def get_all(self) -> List[T]:
+        return self.__items
+
+# Сейф для чисел
+int_storage = Storage[int]()
+int_storage.put(10)
+int_storage.put(20)
+print(int_storage.get_all)
 
 
-def show_info_v(figure: Figure):
-    print(figure.create_v())
+# Сейф для букв
 
+str_storage = Storage[str]()
+str_storage.put("hello")
+str_storage.put("bye")
 
-
-figures = [Sphere(), Cube(), Piramid()]
-
-for _ in figures:
-    show_info_v(_)
+print(f"first storage: {int_storage.get_all}")
+print(f"second storage: {str_storage.get_all}")
